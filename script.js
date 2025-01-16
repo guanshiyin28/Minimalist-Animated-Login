b0ea907d6504d29cd3f36b7ff528af68d16b0d45
Guan <guanshiyinnn@gmail.com>
2025-01-16 11:06:27 +0700

    Rearrange Text

--- script.js
+++ script.js
@@ -1,11 +1,11 @@
-const container = document.querySelector('.container');
-const signupButton = document.querySelector('.signup-section header');
-const loginButton = document.querySelector('.login-section header');
+const container = document.querySelector(".container");
+const signupButton = document.querySelector(".signup-section header");
+const loginButton = document.querySelector(".login-section header");
 
-loginButton.addEventListener('click', () => {
-    container.classList.add('active');
+loginButton.addEventListener("click", () => {
+  container.classList.add("active");
 });
 
-signupButton.addEventListener('click', () => {
-    container.classList.remove('active');
+signupButton.addEventListener("click", () => {
+  container.classList.remove("active");
 });
